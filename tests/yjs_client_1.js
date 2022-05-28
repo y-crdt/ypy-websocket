1c6e159d7cf2ee189cc4e065492c0bc80e6f5585
David Brochart <david.brochart@gmail.com>
2022-05-28 10:14:47 +0200

    Fix WebsocketServer test

--- tests/yjs_client_1.js
+++ tests/yjs_client_1.js
@@ -1,14 +1,15 @@
 const Y = require('yjs')
 const WebsocketProvider = require('y-websocket').WebsocketProvider
 
-const doc = new Y.Doc()
-const ycells = doc.getArray("cells")
-const ystate = doc.getMap("state")
+const ydoc = new Y.Doc()
+const ytest = ydoc.getMap('_test')
+const ycells = ydoc.getArray("cells")
+const ystate = ydoc.getMap("state")
 const ws = require('ws')
 
 const wsProvider = new WebsocketProvider(
   'ws://localhost:1234', 'my-roomname',
-  doc,
+  ydoc,
   { WebSocketPolyfill: ws }
 )
 
@@ -16,10 +17,19 @@ wsProvider.on('status', event => {
   console.log(event.status)
 })
 
-ycells.observe(event => {
-  console.log("ycell changed")
-})
+var clock = -1
 
-ystate.observe(event => {
-  console.log("ystate changed")
+ytest.observe(event => {
+  event.changes.keys.forEach((change, key) => {
+    if (key === 'clock') {
+      const clk = ytest.get('clock')
+      if (clk > clock) {
+        const cells = [new Y.Map([['source', new Y.Text('1 + 2')], ['metadata', {'foo': 'bar'}]])]
+        ycells.push(cells)
+        ystate.set('state', {'dirty': false})
+        clock = clk + 1
+        ytest.set('clock', clock)
+      }
+    }
+  })
 })
