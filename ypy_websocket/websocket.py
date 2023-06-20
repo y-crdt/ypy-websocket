930ec4956bea9954b7ce06acfa66629a0273736f
David Brochart <david.brochart@gmail.com>
2023-06-20 07:56:40 +0200

    Install typing_extensions for python<3.8 (#82)

--- ypy_websocket/websocket.py
+++ ypy_websocket/websocket.py
@@ -1,4 +1,9 @@
-from typing_extensions import Protocol
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+else:
+    from typing_extensions import Protocol
 
 
 class Websocket(Protocol):
