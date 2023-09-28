bdd4f9efd23ea69e432545a614ea4048b2a1db9b
Carlos Herrero <26092748+hbcarlos@users.noreply.github.com>
2023-09-28 15:28:09 +0200

    Updates the SQLite store

---
+++ tests/test_sqlite_store.py
@@ -0,0 +1,309 @@
+import pytest
+
+import time
+import aiosqlite
+import y_py as Y
+
+from ypy_websocket.stores import SQLiteYStore, DocExists
+
+@pytest.fixture
+def create_database():
+    async def _inner(path: str, version: int, tables: bool = True) -> None:
+        async with aiosqlite.connect(path) as db:
+            if tables:
+                await db.execute("CREATE TABLE IF NOT EXISTS documents (path TEXT PRIMARY KEY, version INTEGER NOT NULL)")
+                await db.execute("CREATE TABLE IF NOT EXISTS yupdates (path TEXT NOT NULL, yupdate BLOB, metadata BLOB, timestamp REAL NOT NULL)")
+                await db.execute("CREATE INDEX IF NOT EXISTS idx_yupdates_path_timestamp ON yupdates (path, timestamp)")
+            await db.execute(f"PRAGMA user_version = {version}")
+            await db.commit()
+    
+    return _inner
+
+@pytest.fixture
+def add_document():
+    async def _inner(path: str, doc_path: str, version: int, data: bytes = b"") -> None:
+        async with aiosqlite.connect(path) as db:
+            await db.execute("INSERT INTO documents VALUES (?, ?)", (doc_path, version),)
+            await db.execute("INSERT INTO yupdates VALUES (?, ?, ?, ?)", (doc_path, data, b"", time.time()),)
+            await db.commit()
+    
+    return _inner
+
+@pytest.mark.anyio
+async def test_initialization(tmp_path):
+    path = tmp_path / "tmp.db"
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    async with aiosqlite.connect(path) as db:
+        cursor = await db.execute("pragma user_version")
+        version = (await cursor.fetchone())[0]
+        assert store.version == version
+
+        cursor = await db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='documents'")
+        res = await cursor.fetchone()
+        assert res[0] == 1
+
+        cursor = await db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='yupdates'")
+        res = await cursor.fetchone()
+        assert res[0] == 1
+
+@pytest.mark.anyio
+async def test_initialization_with_old_database(tmp_path, create_database):
+    path = tmp_path / "tmp.db"
+
+    # Create a database with an old version
+    await create_database(path, 1)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    async with aiosqlite.connect(path) as db:
+        cursor = await db.execute("pragma user_version")
+        version = (await cursor.fetchone())[0]
+        assert store.version == version
+
+        cursor = await db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='documents'")
+        res = await cursor.fetchone()
+        assert res[0] == 1
+
+        cursor = await db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='yupdates'")
+        res = await cursor.fetchone()
+        assert res[0] == 1
+    
+@pytest.mark.anyio
+async def test_initialization_with_empty_database(tmp_path, create_database):
+    path = tmp_path / "tmp.db"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version, False)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    async with aiosqlite.connect(path) as db:
+        cursor = await db.execute("pragma user_version")
+        version = (await cursor.fetchone())[0]
+        assert store.version == version
+
+        cursor = await db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='documents'")
+        res = await cursor.fetchone()
+        assert res[0] == 1
+
+        cursor = await db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='yupdates'")
+        res = await cursor.fetchone()
+        assert res[0] == 1
+
+@pytest.mark.anyio
+async def test_initialization_with_existing_database(tmp_path, create_database, add_document):
+    path = tmp_path / "tmp.db"
+    doc_path = "test.txt"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version)
+    await add_document(path, doc_path, 0)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    async with aiosqlite.connect(path) as db:
+        cursor = await db.execute("pragma user_version")
+        version = (await cursor.fetchone())[0]
+        assert store.version == version
+
+        cursor = await db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='documents'")
+        res = await cursor.fetchone()
+        assert res[0] == 1
+
+        cursor = await db.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='yupdates'")
+        res = await cursor.fetchone()
+        assert res[0] == 1
+
+        cursor = await db.execute("SELECT path, version FROM documents WHERE path = ?", (doc_path,),)
+        res = await cursor.fetchone()
+        assert res[0] == doc_path
+        assert res[1] == 0
+
+@pytest.mark.anyio
+async def test_exists(tmp_path, create_database, add_document):
+    path = tmp_path / "tmp.db"
+    doc_path = "test.txt"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version)
+    await add_document(path, doc_path, 0)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    res = await store.exists(doc_path)
+    assert res == True
+
+    res = await store.exists("random.path")
+    assert res == False
+
+@pytest.mark.anyio
+async def test_list(tmp_path, create_database, add_document):
+    path = tmp_path / "tmp.db"
+    doc1 = "test_1.txt"
+    doc2 = "test_2.txt"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version)
+    await add_document(path, doc1, 0)
+    await add_document(path, doc2, 0)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    count = 0
+    async for doc in store.list():
+        count += 1
+        assert doc in [doc1, doc2]
+    
+    assert count == 2
+
+@pytest.mark.anyio
+async def test_get(tmp_path, create_database, add_document):
+    path = tmp_path / "tmp.db"
+    doc_path = "test.txt"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version)
+    await add_document(path, doc_path, 0)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    res = await store.get(doc_path)
+    assert res["path"] == doc_path
+    assert res["version"] == 0
+
+    res = await store.get("random.doc")
+    assert res == None
+
+@pytest.mark.anyio
+async def test_create(tmp_path, create_database, add_document):
+    path = tmp_path / "tmp.db"
+    doc_path = "test.txt"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version)
+    await add_document(path, doc_path, 0)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    new_doc = "new_doc.path"
+    await store.create(new_doc, 0)
+    async with aiosqlite.connect(path) as db:
+        cursor = await db.execute("SELECT path, version FROM documents WHERE path = ?", (new_doc,),)
+        res = await cursor.fetchone()
+        assert res[0] == new_doc
+        assert res[1] == 0
+
+    with pytest.raises(DocExists) as e:  
+        await store.create(doc_path, 0)
+    assert str(e.value) == f"The document {doc_path} already exists."
+    
+@pytest.mark.anyio
+async def test_remove(tmp_path, create_database, add_document):
+    path = tmp_path / "tmp.db"
+    doc_path = "test.txt"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version)
+    await add_document(path, doc_path, 0)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    await store.remove(doc_path)
+    res = await store.exists(doc_path)
+    assert res == False
+
+    new_doc = "new_doc.path"
+    res = await store.exists(new_doc)
+    assert res == False
+
+    await store.remove(new_doc)
+    res = await store.exists(new_doc)
+    assert res == False
+
+@pytest.mark.anyio
+async def test_read(tmp_path, create_database, add_document):
+    path = tmp_path / "tmp.db"
+    doc_path = "test.txt"
+    update = b"foo"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version)
+    await add_document(path, doc_path, 0, update)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    count = 0
+    async for u,_,_ in store.read(doc_path):
+        count += 1
+        assert update == u
+    
+    assert count == 1
+
+@pytest.mark.anyio
+async def test_write(tmp_path, create_database, add_document):
+    path = tmp_path / "tmp.db"
+    doc_path = "test.txt"
+
+    # Create a database with an old version
+    await create_database(path, SQLiteYStore.version)
+    await add_document(path, doc_path, 0)
+
+    store = SQLiteYStore(str(path))
+    await store.start()
+    await store.initialize()
+
+    assert store.initialized == True
+
+    update = b"foo"
+    await store.write(doc_path, update)
+    
+    async with aiosqlite.connect(path) as db:
+        async with db.execute("SELECT yupdate FROM yupdates WHERE path = ?", (doc_path,)) as cursor:
+            count = 0
+            async for u, in cursor:
+                count += 1
+                # The fixture add_document inserts an empty update
+                assert u in [b"", update]
+            assert count == 2
+    
--- ypy_websocket/stores/sqlite_store.py
+++ ypy_websocket/stores/sqlite_store.py
@@ -12,9 +12,9 @@ from anyio.abc import TaskStatus
 
 import y_py as Y
 
-from .yutils import get_new_path
+from ..yutils import get_new_path
 from .base_store import BaseYStore
-from .utils import YDocNotFound
+from .utils import YDocNotFound, DocExists
 
 class SQLiteYStore(BaseYStore):
     """A YStore which uses an SQLite database.
@@ -24,37 +24,33 @@ class SQLiteYStore(BaseYStore):
 
     ```py
     class MySQLiteYStore(SQLiteYStore):
-        db_path = "path/to/my_ystore.db"
+        _store_path = "path/to/my_ystore.db"
     ```
     """
 
-    db_path: str = "ystore.db"
+    _lock: Lock
     # Determines the "time to live" for all documents, i.e. how recent the
     # latest update of a document must be before purging document history.
     # Defaults to never purging document history (None).
     document_ttl: int | None = None
-    path: str
-    lock: Lock
-    db_initialized: Event
 
     def __init__(
         self,
-        path: str,
+        path: str = "./ystore.db",
         metadata_callback: Callable[[], Awaitable[bytes] | bytes] | None = None,
         log: Logger | None = None,
     ) -> None:
         """Initialize the object.
 
         Arguments:
-            path: The file path used to store the updates.
+            path: The database path used to store the updates.
             metadata_callback: An optional callback to call to get the metadata.
             log: An optional logger.
         """
-        self.path = path
+        self._lock = Lock()
+        self._store_path = path
         self.metadata_callback = metadata_callback
         self.log = log or getLogger(__name__)
-        self.lock = Lock()
-        self.db_initialized = Event()
 
     async def start(self, *, task_status: TaskStatus[None] = TASK_STATUS_IGNORED):
         """Start the SQLiteYStore.
@@ -70,62 +66,149 @@ class SQLiteYStore(BaseYStore):
         if self._task_group is not None:
             raise RuntimeError("YStore already running")
 
-        async with create_task_group() as self._task_group:
-            self._task_group.start_soon(self._init_db)
-            self.started.set()
-            self._starting = False
-            task_status.started()
+        self._task_group = create_task_group()
+        self.started.set()
+        self._starting = False
+        task_status.started()
 
-    async def _init_db(self):
-        create_db = False
-        move_db = False
-        if not await anyio.Path(self.db_path).exists():
-            create_db = True
-        else:
-            async with self.lock:
-                async with aiosqlite.connect(self.db_path) as db:
-                    cursor = await db.execute(
-                        "SELECT count(name) FROM sqlite_master WHERE type='table' and name='yupdates'"
-                    )
-                    table_exists = (await cursor.fetchone())[0]
-                    if table_exists:
-                        cursor = await db.execute("pragma user_version")
-                        version = (await cursor.fetchone())[0]
-                        if version != self.version:
-                            move_db = True
-                            create_db = True
-                    else:
-                        create_db = True
-        if move_db:
-            new_path = await get_new_path(self.db_path)
-            self.log.warning(f"YStore version mismatch, moving {self.db_path} to {new_path}")
-            await anyio.Path(self.db_path).rename(new_path)
-        if create_db:
-            async with self.lock:
-                async with aiosqlite.connect(self.db_path) as db:
-                    await db.execute(
-                        "CREATE TABLE yupdates (path TEXT NOT NULL, yupdate BLOB, metadata BLOB, timestamp REAL NOT NULL)"
-                    )
-                    await db.execute(
-                        "CREATE INDEX idx_yupdates_path_timestamp ON yupdates (path, timestamp)"
-                    )
-                    await db.execute(f"PRAGMA user_version = {self.version}")
+    async def initialize(self) -> None:
+        """
+        Initializes the store.
+        """
+        if self.initialized or self._initialized is not None:
+            return
+        self._initialized = Event() 
+
+        async with self._lock:
+            if await anyio.Path(self._store_path).exists():
+                version = -1
+                async with aiosqlite.connect(self._store_path) as db:
+                    cursor = await db.execute("pragma user_version")
+                    version = (await cursor.fetchone())[0]
+                    
+                # The DB has an old version. Move the database.
+                if self.version != version:
+                    new_path = await get_new_path(self._store_path)
+                    self.log.warning(f"YStore version mismatch, moving {self._store_path} to {new_path}")
+                    await anyio.Path(self._store_path).rename(new_path)
+
+            # Make sure every table exists.
+            async with aiosqlite.connect(self._store_path) as db:
+                await db.execute("CREATE TABLE IF NOT EXISTS documents (path TEXT PRIMARY KEY, version INTEGER NOT NULL)")
+                await db.execute("CREATE TABLE IF NOT EXISTS yupdates (path TEXT NOT NULL, yupdate BLOB, metadata BLOB, timestamp REAL NOT NULL)")
+                await db.execute("CREATE INDEX IF NOT EXISTS idx_yupdates_path_timestamp ON yupdates (path, timestamp)")
+                await db.execute(f"PRAGMA user_version = {self.version}")
+                await db.commit()
+                
+        self._initialized.set()
+    
+    async def exists(self, path: str) -> bool:
+        """
+        Returns True if the document exists, else returns False.
+
+        Arguments:
+            path: The document name/path.
+        """
+        if self._initialized is None:
+            raise Exception("The store was not initialized.")
+        await self._initialized.wait()
+
+        async with self._lock:
+            async with aiosqlite.connect(self._store_path) as db:
+                cursor = await db.execute("SELECT path, version FROM documents WHERE path = ?", (path,),)
+                return (await cursor.fetchone()) is not None
+    
+    async def list(self) -> AsyncIterator[str]:
+        """
+        Returns a list with the name/path of the documents stored.
+        """
+        if self._initialized is None:
+            raise Exception("The store was not initialized.")
+        await self._initialized.wait()
+
+        async with self._lock:
+            async with aiosqlite.connect(self._store_path) as db:
+                async with db.execute("SELECT path FROM documents") as cursor:
+                    async for path in cursor:
+                        yield path[0]
+    
+    async def get(self, path: str) -> dict | None:
+        """
+        Returns the document's metadata or None if the document does't exist.
+
+        Arguments:
+            path: The document name/path.
+        """
+        if self._initialized is None:
+            raise Exception("The store was not initialized.")
+        await self._initialized.wait()
+
+        async with self._lock:
+            async with aiosqlite.connect(self._store_path) as db:
+                cursor = await db.execute("SELECT path, version FROM documents WHERE path = ?", (path,),)
+                doc = await cursor.fetchone()
+
+                if doc is None:
+                    return None
+                else :
+                    return dict(path=doc[0], version=doc[1])
+        
+    async def create(self, path: str, version: int) -> None:
+        """
+        Creates a new document.
+
+        Arguments:
+            path: The document name/path.
+            version: Document version.
+        """
+        if self._initialized is None:
+            raise Exception("The store was not initialized.")
+        await self._initialized.wait()
+
+        async with self._lock:
+            try:
+                async with aiosqlite.connect(self._store_path) as db:
+                    await db.execute("INSERT INTO documents VALUES (?, ?)", (path, version),)
                     await db.commit()
-        self.db_initialized.set()
+            except aiosqlite.IntegrityError:
+                raise DocExists(f"The document {path} already exists.")
+    
+    async def remove(self, path: str) -> None:
+        """
+        Removes a document.
 
-    async def read(self) -> AsyncIterator[tuple[bytes, bytes, float]]:  # type: ignore
+        Arguments:
+            path: The document name/path.
+        """
+        if self._initialized is None:
+            raise Exception("The store was not initialized.")
+        await self._initialized.wait()
+
+        async with self._lock:
+            async with aiosqlite.connect(self._store_path) as db:
+                await db.execute("DELETE FROM documents WHERE path = ?", (path,),)
+                await db.execute("DELETE FROM yupdates WHERE path = ?", (path,),)
+                await db.commit()
+
+    async def read(self, path: str) -> AsyncIterator[tuple[bytes, bytes, float]]:  # type: ignore
         """Async iterator for reading the store content.
+
+        Arguments:
+            path: The document name/path.
 
         Returns:
             A tuple of (update, metadata, timestamp) for each update.
         """
-        await self.db_initialized.wait()
+        if self._initialized is None:
+            raise Exception("The store was not initialized.")
+        await self._initialized.wait()
+
         try:
-            async with self.lock:
-                async with aiosqlite.connect(self.db_path) as db:
+            async with self._lock:
+                async with aiosqlite.connect(self._store_path) as db:
                     async with db.execute(
                         "SELECT yupdate, metadata, timestamp FROM yupdates WHERE path = ?",
-                        (self.path,),
+                        (path,),
                     ) as cursor:
                         found = False
                         async for update, metadata, timestamp in cursor:
@@ -136,19 +219,24 @@ class SQLiteYStore(BaseYStore):
         except Exception:
             raise YDocNotFound
 
-    async def write(self, data: bytes) -> None:
-        """Store an update.
+    async def write(self, path: str, data: bytes) -> None:
+        """
+        Store an update.
 
         Arguments:
+            path: The document name/path.
             data: The update to store.
         """
-        await self.db_initialized.wait()
-        async with self.lock:
-            async with aiosqlite.connect(self.db_path) as db:
+        if self._initialized is None:
+            raise Exception("The store was not initialized.")
+        await self._initialized.wait()
+
+        async with self._lock:
+            async with aiosqlite.connect(self._store_path) as db:
                 # first, determine time elapsed since last update
                 cursor = await db.execute(
                     "SELECT timestamp FROM yupdates WHERE path = ? ORDER BY timestamp DESC LIMIT 1",
-                    (self.path,),
+                    (path,),
                 )
                 row = await cursor.fetchone()
                 diff = (time.time() - row[0]) if row else 0
@@ -157,24 +245,24 @@ class SQLiteYStore(BaseYStore):
                     # squash updates
                     ydoc = Y.YDoc()
                     async with db.execute(
-                        "SELECT yupdate FROM yupdates WHERE path = ?", (self.path,)
+                        "SELECT yupdate FROM yupdates WHERE path = ?", (path,)
                     ) as cursor:
                         async for update, in cursor:
                             Y.apply_update(ydoc, update)
                     # delete history
-                    await db.execute("DELETE FROM yupdates WHERE path = ?", (self.path,))
+                    await db.execute("DELETE FROM yupdates WHERE path = ?", (path,))
                     # insert squashed updates
                     squashed_update = Y.encode_state_as_update(ydoc)
                     metadata = await self.get_metadata()
                     await db.execute(
                         "INSERT INTO yupdates VALUES (?, ?, ?, ?)",
-                        (self.path, squashed_update, metadata, time.time()),
+                        (path, squashed_update, metadata, time.time()),
                     )
 
                 # finally, write this update to the DB
                 metadata = await self.get_metadata()
                 await db.execute(
                     "INSERT INTO yupdates VALUES (?, ?, ?, ?)",
-                    (self.path, data, metadata, time.time()),
+                    (path, data, metadata, time.time()),
                 )
                 await db.commit()
