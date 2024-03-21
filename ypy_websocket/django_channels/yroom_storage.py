a59b7a1dfc62484a16816a8cf632779df82a30fa
cacosandon <jiossandon@uc.cl>
2024-03-21 16:10:20 -0300

    feat(yroom_storage): add Redis storage as an example

--- ypy_websocket/django_channels/yroom_storage.py
+++ ypy_websocket/django_channels/yroom_storage.py
@@ -1,6 +1,9 @@
 import time
+from typing import Optional
 
+import redis.asyncio as redis
 import y_py as Y
+
 
 class BaseYRoomStorage:
     def __init__(self, room_name) -> None:
@@ -77,3 +80,82 @@ class BaseYRoomStorage:
         return Y.encode_state_as_update(document)
 
 
+
+
+class RedisYRoomStorage(BaseYRoomStorage):
+    """A YRoom storage that uses Redis as main storage, without
+    persistent storage.
+
+    Args:
+        room_name: The name of the room.
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.redis_key = f"document:{self.room_name}"
+        self.redis = self.make_redis()
+
+    def make_redis(self):
+        """Makes a Redis client.
+
+        Defaults to a local client"""
+
+        return redis.Redis(host="localhost", port=6379, db=0)
+
+    async def get_document(self) -> Y.YDoc:
+        snapshot = await self.redis.get(self.redis_key)
+
+        if not snapshot:
+            snapshot = await self.get_snapshot_from_database()
+
+        document = Y.YDoc()
+
+        if snapshot:
+            Y.apply_update(document, snapshot)
+
+        return document
+
+    async def get_snapshot_from_database(self) -> Optional[bytes]:
+        """Gets the document from the database. Override this method to
+        implement a persistent storage.
+
+        Defaults to None.
+
+        Returns:
+            (Optional) The document with the latest changes.
+        """
+        return None
+
+    async def update_document(self, update: bytes):
+        await self.redis.watch(self.redis_key)
+
+        try:
+            current_document = await self.get_document()
+            updated_snapshot = self._apply_update_to_snapshot(current_document, update)
+
+            async with self.redis.pipeline() as pipe:
+                while True:
+                    try:
+                        pipe.multi()
+                        pipe.set(self.redis_key, updated_snapshot)
+
+                        await pipe.execute()
+
+                        break
+                    except redis.WatchError:
+                        current_snapshot = await self.get_document()
+                        updated_snapshot = self._apply_update_to_snapshot(
+                            current_snapshot,
+                            update,
+                        )
+
+                        continue
+        finally:
+            await self.redis.unwatch()
+
+        await self.debounced_persist_document()
+
+    async def close(self):
+        await self.persist_document()
+        await self.redis.close()
